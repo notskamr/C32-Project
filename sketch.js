c466c1c4e5be329dcf220f88902d0132c36aeb55
notskamr <sahnivarun62@gmail.com>
2020-08-10 18:42:30 +0530

    0

--- sketch.js
+++ sketch.js
@@ -133,3 +133,21 @@ function mouseReleased(){
 
   slingShot.fly();
 }
+
+async function getBackgroundImg(){
+  var response = await fetch("http://worldtimeapi.org/api/ip");
+  var responseJSON = await response.json();
+
+  var datetime = responseJSON.datetime;
+  var hour = datetime.slice(11,13);
+  
+  if(hour>=0600 && hour<=1900){
+      bg = "sprites/bg1.png";
+  }
+  else{
+      bg = "sprites/bg2.jpg";
+  }
+
+  backgroundImg = loadImage(bg);
+  console.log(backgroundImg);
+}
